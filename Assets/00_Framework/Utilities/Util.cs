46ddd77b091fc9341c968b15104ed50404b44ae7
Eunseok <79277772+Eunseok@users.noreply.github.com>
2025-03-10 12:32:56 +0900

    feat: StarterAssets 적용

--- Assets/00_Framework/Utilities/Util.cs
+++ Assets/00_Framework/Utilities/Util.cs
@@ -42,5 +42,13 @@ namespace DefaultNamespace
 
             return null;
         }
+        
+        
+        public static float ClampAngle(float lfAngle, float lfMin, float lfMax)
+        {
+            if (lfAngle < -360f) lfAngle += 360f;
+            if (lfAngle > 360f) lfAngle -= 360f;
+            return Mathf.Clamp(lfAngle, lfMin, lfMax);
+        }
     }
 }
--- Assets/01_Scripts/UI/HUD/Conditions/UICondition.cs
+++ Assets/01_Scripts/UI/HUD/Conditions/UICondition.cs
@@ -6,16 +6,18 @@ namespace Scripts.UI.Condition
     public class UICondition : MonoBehaviour
     {
         [SerializeField]private UIConditionBar healthBar;
-        [SerializeField]private UIConditionBar hungerBar;
         [SerializeField]private UIConditionBar staminaBar;
 
 
         private void Start()
         {
-            StatHandler playerStatHandler = CharacterManager.Instance.Player.statHandler;
-            playerStatHandler.health.OnValueChangedUnityEvent += healthBar.SetCondition;
-            playerStatHandler.hunger.OnValueChangedUnityEvent += hungerBar.SetCondition;
-            playerStatHandler.stamina.OnValueChangedUnityEvent += staminaBar.SetCondition;
+            Player player = CharacterManager.Instance.Player;
+            if (player != null)
+            {
+                StatHandler playerStatHandler = player.statHandler;
+                playerStatHandler.health.OnValueChangedUnityEvent += healthBar.SetCondition;
+                playerStatHandler.stamina.OnValueChangedUnityEvent += staminaBar.SetCondition;
+            }
         }
     }
 }
